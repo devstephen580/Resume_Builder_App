adec74c6107ffbcd41e738f087b8f58eebf23f3a
devstephen580 <stephendev580@gmail.com>
2026-04-02 22:51:36 +0100

    feature: Added JwtAuthEntryPoint

--- src/main/java/com/devstephen/resume_app_sp/config/SecurityConfig.java
+++ src/main/java/com/devstephen/resume_app_sp/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.devstephen.resume_app_sp.config;
 
+import com.devstephen.resume_app_sp.jwtconfig.JwtAuthEntryPoint;
+import com.devstephen.resume_app_sp.jwtconfig.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -19,13 +23,19 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthFilter authFilter;
+
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http){
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(new JwtAuthEntryPoint()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf( csrf -> csrf.disable())
                 .authorizeHttpRequests(request ->
                         request.requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/verify-email", "/api/auth/upload-image").permitAll()
                         .anyRequest().authenticated());
+
         return http.build();
 
     }
---
+++ src/main/java/com/devstephen/resume_app_sp/jwtconfig/JwtAuthEntryPoint.java
@@ -0,0 +1,28 @@
+package com.devstephen.resume_app_sp.jwtconfig;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", "Not authorized, please provide token.");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), errorResponse);
+    }
+}
