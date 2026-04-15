51f8e0b245da0c4cdaf53ba3d8a61a2901b86c3b
devstephen580 <stephendev580@gmail.com>
2026-04-15 10:55:23 +0100

    feat: add PaymentResponse DTO

--- src/main/java/com/devstephen/resume_app_sp/dto/PaymentResponse.java
+++ src/main/java/com/devstephen/resume_app_sp/dto/PaymentResponse.java
@@ -1,4 +1,19 @@
 package com.devstephen.resume_app_sp.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PaymentResponse {
+    private String authorizationUrl;
+    private String receipt;
+    private int amount;
+    private String currency;
+    private String planType;
+    private String status;
+
+    private String paystackOrderId;
+
+
 }
